78a673227744c5155a6ad020bfde4ed27969a7ba
Ngoc Tuyen <tranngoctuyen019b@gmail.com>
2026-06-28 12:53:09 +0700

    expectimax

--- algorithms/Adversarial_Search/__init__.py
+++ algorithms/Adversarial_Search/__init__.py
@@ -1,4 +1,5 @@
 #from .Alpha_Beta import solve_alphabeta
-
+#from .Minimax import solve_minimax
+from .Expectimax import solve_expectimax
 
 __all__ = ["solve_alphabeta", "solve_minimax", "solve_expectimax"]
--- gui/Main_Window.py
+++ gui/Main_Window.py
@@ -11,7 +11,7 @@ from algorithms.Uninformed_Search   import solve_bfs , solve_dfs#, solve_ucs
 from algorithms.Informed_Search     import solve_idastar, solve_greedy #, solve_astar
 from algorithms.Local_Search        import solve_hill_climbing , solve_simulated_annealing#, solve_local_beam
 from algorithms.Constraint_Satisfaction_Problems     import solve_backtracking , solve_bomb_forward_checking#, solve_bomb_min_conflicts
-#from algorithms.Adversarial_Search      import  solve_alphabeta#, solve_minimax, solve_expectimax
+from algorithms.Adversarial_Search      import  solve_expectimax#,solve_alphabeta, solve_minimax
 from algorithms.Complex_Environments    import solve_partial_observation, solve_and_or#, solve_no_observation
 
 MAP_OPTIONS = [
@@ -69,10 +69,10 @@ ALGO_GROUPS = [
      "algos": [("Backtracking", solve_backtracking),
                ("Forward Checking", solve_bomb_forward_checking),]},
                #("Min-Conflicts", solve_min_conflicts)]},
-   # {"name": "Adversarial Search","color": "#e94560",
-    # "algos": [("Alpha-Beta", solve_alphabeta),]},
+    {"name": "Adversarial Search","color": "#e94560",
+    "algos": [#("Alpha-Beta", solve_alphabeta),]},
                 #("Minimax", solve_minimax),
-               #("Expectimax", solve_expectimax)]},
+               ("Expectimax", solve_expectimax)]},
     {"name": "Belief State",      "color": "#fd79a8",
      "algos": [("Partial Observation", solve_partial_observation),
                 ("AND-OR Graph", solve_and_or)]},#, ("Sensorless", solve_no_observation)]},
